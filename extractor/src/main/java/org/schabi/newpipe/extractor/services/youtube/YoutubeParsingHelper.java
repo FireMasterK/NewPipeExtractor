95d2189ec2708f6a4fc9926fe595cdf7e27751a7
ThetaDev <t.testboy@gmail.com>
2022-10-25 09:00:32 +0200

    fix: store YouTube visitor data for channel tabs

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/YoutubeParsingHelper.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/YoutubeParsingHelper.java
@@ -1225,7 +1225,8 @@ public final class YoutubeParsingHelper {
     @Nonnull
     public static JsonBuilder<JsonObject> prepareDesktopJsonBuilder(
             @Nonnull final Localization localization,
-            @Nonnull final ContentCountry contentCountry)
+            @Nonnull final ContentCountry contentCountry,
+            @Nullable final String vData)
             throws IOException, ExtractionException {
         // @formatter:off
         final JsonBuilder<JsonObject> builder = JsonObject.builder()
@@ -1238,7 +1239,10 @@ public final class YoutubeParsingHelper {
                         .value("originalUrl", "https://www.youtube.com")
                         .value("platform", "DESKTOP");
 
-        if (visitorData != null) {
+        // Use specified visitor data, otherwise fall back to the configured value
+        if (vData != null) {
+            builder.value("visitorData", vData);
+        } else if (visitorData != null) {
             builder.value("visitorData", visitorData);
         }
 
@@ -1257,6 +1261,14 @@ public final class YoutubeParsingHelper {
         // @formatter:on
 
         return builder;
+    }
+
+    @Nonnull
+    public static JsonBuilder<JsonObject> prepareDesktopJsonBuilder(
+            @Nonnull final Localization localization,
+            @Nonnull final ContentCountry contentCountry)
+            throws IOException, ExtractionException {
+        return prepareDesktopJsonBuilder(localization, contentCountry, visitorData);
     }
 
     @Nonnull
@@ -1775,7 +1787,8 @@ public final class YoutubeParsingHelper {
     public static ChannelResponseData getChannelResponse(final String channelId,
                                                          final String params,
                                                          final Localization loc,
-                                                         final ContentCountry country)
+                                                         final ContentCountry country,
+                                                         @Nullable final String vData)
             throws ExtractionException, IOException {
         String id = channelId;
         JsonObject ajaxJson = null;
@@ -1783,7 +1796,7 @@ public final class YoutubeParsingHelper {
         int level = 0;
         while (level < 3) {
             final byte[] body = JsonWriter.string(prepareDesktopJsonBuilder(
-                            loc, country)
+                            loc, country, vData)
                             .value("browseId", id)
                             .value("params", params) // Equal to videos
                             .done())
--- extractor/src/test/java/org/schabi/newpipe/extractor/services/youtube/stream/YoutubeStreamExtractorDefaultTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/youtube/stream/YoutubeStreamExtractorDefaultTest.java
@@ -49,6 +49,7 @@ import org.schabi.newpipe.extractor.stream.StreamExtractor;
 import org.schabi.newpipe.extractor.stream.StreamSegment;
 import org.schabi.newpipe.extractor.stream.StreamType;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -56,7 +57,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import javax.annotation.Nullable;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.schabi.newpipe.extractor.ServiceList.YouTube;
 
 public class YoutubeStreamExtractorDefaultTest {
     private static final String RESOURCE_PATH = DownloaderFactory.RESOURCE_PATH + "services/youtube/extractor/stream/";
